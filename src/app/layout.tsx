57653ab321ebea281cac6f02d7af75cb3cf2c3a1
HyeongSeok Yoon <sjallk@gmail.com>
2026-03-15 20:00:07 +0900

    feat: 폰트 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+
+import { Noto_Sans_KR, Nanum_Gothic_Coding } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const notoSansKR = Noto_Sans_KR({
+  variable: "--font-noto-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const nanumGothicCoding = Nanum_Gothic_Coding({
+  variable: "--font-nanum-mono",
   subsets: ["latin"],
+  weight: ["400", "700"],
 });
 
 export const metadata: Metadata = {
@@ -23,9 +25,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${notoSansKR.variable} ${nanumGothicCoding.variable} antialiased`}
       >
         {children}
       </body>
