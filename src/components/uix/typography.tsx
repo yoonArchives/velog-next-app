4b0512ed65807efb845daba38bdc8343fefaa8d8
HyeongSeok Yoon <sjallk@gmail.com>
2026-04-05 21:45:59 +0900

    feat: 시맨틱 태그를 적용한 Typography(H1-H4) 컴포넌트 추가

---
+++ src/components/uix/typography.tsx
@@ -0,0 +1,78 @@
+import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
+
+export function TypographyH1({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) {
+  return (
+    <h1
+      className={cn(
+        "scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance",
+        className,
+      )}
+    >
+      {children}
+    </h1>
+  );
+}
+
+export function TypographyH2({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) {
+  return (
+    <h2
+      className={cn(
+        "scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0",
+        className,
+      )}
+    >
+      {children}
+    </h2>
+  );
+}
+
+export function TypographyH3({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) {
+  return (
+    <h3
+      className={cn(
+        "scroll-m-20 text-2xl font-semibold tracking-tight",
+        className,
+      )}
+    >
+      {children}
+    </h3>
+  );
+}
+
+export function TypographyH4({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) {
+  return (
+    <h4
+      className={cn(
+        "scroll-m-20 text-xl font-semibold tracking-tight",
+        className,
+      )}
+    >
+      {children}
+    </h4>
+  );
+}
